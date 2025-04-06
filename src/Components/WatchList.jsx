21313b81cfb6de60a6b7a278df7b7bf2c3f8394a
Jinesh Gandhi <jineshgandhi59@gmail.com>
2025-04-07 01:29:42 +0530

    filters

--- src/Components/WatchList.jsx
+++ src/Components/WatchList.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./WatchList.css";
 import WatchListTable from "./WatchListTable";
 import {Link} from 'react-router-dom'
 import { useState } from "react";
+import { genreids } from "../utilities/genre";
 
 function WatchList({ watchlist, removeFromWatchList }) {
 
   const [search,setSearch] = useState("");
+  const [genreList,setGenreList] = useState([]);
+  const [currGenre,setCurrGenre] = useState("All Genres")
+
+  useEffect(()=>{
+    let genreSet = new Set();
+    genreSet.add("All Genres");
+    watchlist.map((movie)=>{
+      genreSet.add(genreids[movie.genre_ids[0]]);
+    })
+    setGenreList([...genreSet]);
+  },[watchlist])
 
   return (
     <>
@@ -18,7 +30,17 @@ function WatchList({ watchlist, removeFromWatchList }) {
                 setSearch(e.target.value);
               }} value={search} type="text"/>
             </div>
-            <WatchListTable search={search} watchlist={watchlist} removeFromWatchList={removeFromWatchList}></WatchListTable>
+            
+
+            <div className="genreList"> 
+            {
+              genreList.map((genre, index) => (
+                <span className={genre==currGenre ? "genre blue" : "genre gray"} onClick={()=>{setCurrGenre(genre);console.log(genre)}} key={index}>{genre}</span>
+              ))
+            }
+            </div>
+
+            <WatchListTable currGenre={currGenre} search={search} watchlist={watchlist} removeFromWatchList={removeFromWatchList}></WatchListTable>
           </>
         ) : (
           <div className="no-movies-container">
--- src/Components/WatchListTable.jsx
+++ src/Components/WatchListTable.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import "./WatchListTable.css"
 import {genreids} from "../utilities/genre.js"
 
-function WatchListTable({watchlist,removeFromWatchList,search}) {
+function WatchListTable({watchlist,removeFromWatchList,search,currGenre}) {
 
   return (
     <table>
@@ -12,12 +12,20 @@ function WatchListTable({watchlist,removeFromWatchList,search}) {
             <th>Ratings</th>
             <th>Popularity</th>
             <th>Genre</th>
-            <th>Delete Movies</th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
           {watchlist.filter((movie)=>{
-            return movie.title.toLowerCase().includes(search.toLowerCase())
+            let searchFilter = movie.title.toLowerCase().includes(search.toLowerCase());
+            let genreFilter;
+            if(currGenre=="All Genres"){
+              genreFilter = true;
+            }
+            else{
+              genreFilter = (genreids[movie.genre_ids[0]]==currGenre);
+            }
+            return genreFilter && searchFilter;
           }).map((movie) => (
             <tr key={movie.id}>
               <td className="name-column">
