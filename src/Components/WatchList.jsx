147ebae9877b67cef60a80f4ea6cb03e59270682
Jinesh Gandhi <jineshgandhi59@gmail.com>
2025-04-06 21:31:14 +0530

    push

--- src/Components/WatchList.jsx
+++ src/Components/WatchList.jsx
@@ -1,55 +1,34 @@
 import React from "react";
 import "./WatchList.css";
+import WatchListTable from "./WatchListTable";
+import {Link} from 'react-router-dom'
 
 function WatchList({ watchlist, removeFromWatchList }) {
 
   return (
     <>
+      {
+        watchlist.length !==0 ? (
+          <>
+            <div className="search">
+              <input placeholder="Search WatchList" type="text"/>
+            </div>
+            <WatchListTable watchlist={watchlist} removeFromWatchList={removeFromWatchList}></WatchListTable>
+          </>
+        ) : (
+          <div className="no-movies-container">
+            <h2>Your Watchlist is Empty 🎬</h2>
+            <p>Looks like you haven't added any movies yet.</p>
+            <Link to="/">
+              <button className="go-movies-btn">Go to Movies Page</button>
+            </Link>
+          </div>
+        )
+      }
 
-      <div className="search">
-        <input placeholder="Search WatchList" type="text"/>
-      </div>
 
-      <table>
-        <thead>
-          <tr>
-            <th className="name-heading">Name</th>
-            <th>Ratings</th>
-            <th>Popularity</th>
-            <th>Genre</th>
-            <th>Delete Movies</th>
-          </tr>
-        </thead>
-        <tbody>
-          {watchlist.map((movie) => (
-            <tr key={movie.id}>
-              <td className="name-column">
-                <div className="name-poster-container">
-                  <img
-                    src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}  `}
-                  />
-                  <span className="movie-title">
-                    {movie.title}
-                  </span>
-                </div>
-              </td>
-              <td>{movie.vote_average}</td>
-              <td>{movie.popularity}</td>
-              <td>{movie.genre_ids.map((genre_id)=>(
-                genre_id + " "
-              ))}</td>
-              <td
-                onClick={() => {
-                  removeFromWatchList(movie);
-                }}
-                className="delete-btn"
-              >
-                Delete
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+
+
     </>
   );
 }
---
+++ src/Components/WatchListTable.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import "./WatchListTable.css"
+
+function WatchListTable({watchlist,removeFromWatchList}) {
+  return (
+    <table>
+        <thead>
+          <tr>
+            <th className="name-heading">Name</th>
+            <th>Ratings</th>
+            <th>Popularity</th>
+            <th>Genre</th>
+            <th>Delete Movies</th>
+          </tr>
+        </thead>
+        <tbody>
+          {watchlist.map((movie) => (
+            <tr key={movie.id}>
+              <td className="name-column">
+                <div className="name-poster-container">
+                  <img
+                    src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}  `}
+                  />
+                  <span className="movie-title">
+                    {movie.title}
+                  </span>
+                </div>
+              </td>
+              <td>{movie.vote_average}</td>
+              <td>{movie.popularity}</td>
+              <td>{movie.genre_ids.map((genre_id)=>(
+                genre_id + " "
+              ))}</td>
+              <td
+                onClick={() => {
+                  removeFromWatchList(movie);
+                }}
+                className="delete-btn"
+              >
+                Delete
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+  )
+}
+
+export default WatchListTable
